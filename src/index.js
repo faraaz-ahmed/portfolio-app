18f269e22adeb198ee3e988a7bf61268023579bc
faraaz-ahmed <faraazahmed000@gmail.com>
2021-11-06 22:54:30 +0530

    fix attempt2

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
+				<App />
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
