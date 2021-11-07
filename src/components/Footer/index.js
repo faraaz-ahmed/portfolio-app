5c3e703f95ab0df6d325d1410d361bb8bc25f930
faraaz-ahmed <faraazahmed000@gmail.com>
2021-11-07 08:15:32 +0530

    feat: add footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
+import linkedInIcon from '../../assets/images/icons8-linkedin.svg';
+import githubIcon from '../../assets/images/icons8-github.svg';
+import twitterIcon from '../../assets/images/icons8-twitter.svg';
 
 const FooterContainer = styled.div`
+	margin: 1rem;
+	display: flex;
+	flex-direction: row;
 	position: absolute;
 	bottom: 1rem;
+	gap: 0.325rem;
+`;
+
+const ImageContainer = styled.div`
+	scale: 7%;
+	max-height: 7%;
+	max-width: 7%;
 `;
 
 const Footer = () => {
@@ -14,10 +27,27 @@ const Footer = () => {
 					target='_blank'
 					rel='noreferrer'
 				>
-					<img
-						src='https://img.icons8.com/fluent/30/000000/twitter.png'
-						alt=''
-					/>
+					<img src={twitterIcon} alt='' />
+				</a>
+				<br></br>
+				<a
+					href='https://www.linkedin.com/in/faraaz-ahmed-286a75141/'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<ImageContainer>
+						<img src={linkedInIcon} alt='' />
+					</ImageContainer>
+				</a>
+				<br></br>
+				<a
+					href='https://github.com/faraaz-ahmed/'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<ImageContainer>
+						<img src={githubIcon} alt='' />
+					</ImageContainer>
 				</a>
 			</FooterContainer>
 		</>
