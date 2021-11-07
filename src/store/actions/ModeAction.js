1ab0333ae0cecc7ae195d7b0f75ce8fa029702db
faraaz-ahmed <faraazahmed000@gmail.com>
2021-11-07 08:16:31 +0530

    Updates

--- src/store/actions/ModeAction.js
+++
@@ -1,9 +0,0 @@
-import { ModeTypes } from '../../AppConstants';
-import { ACTION_TYPE } from '../actiontypes';
-
-export const toggleMode = (currentMode) => {
-	return {
-		type: ACTION_TYPE.CHANGE_MODE,
-		mode: currentMode === ModeTypes.light ? ModeTypes.dark : ModeTypes.light,
-	};
-};
--- src/store/actions/TabsAction.js
+++
@@ -1,9 +0,0 @@
-import { TAB_NAMES } from '../../AppConstants';
-import { ACTION_TYPE } from '../actiontypes';
-
-export const updateTabs = (tab) => {
-	return {
-		type: ACTION_TYPE.UPDATE_TABS,
-		tab: tab ?? TAB_NAMES.ABOUT,
-	};
-};
