a84fe86719db66d250671f111ca52e068edf31cd
faraaz-ahmed <faraazahmed000@gmail.com>
2021-11-07 06:58:37 +0530

    add content to about page

--- src/components/Content/About/index.js
+++ src/components/Content/About/index.js
@@ -45,8 +45,26 @@ const About = () => {
 						</Card.Text> */}
 						<Card.Text>
 							<CustomText size='1rem' colour={globalStyles.textColour}>
-								Software Engineer who focusses on writing clean, elegant and
-								efficient code.
+								<p>
+									Hello! I'm Faraaz Ahmed, a passionate software engineer with a
+									history of working in banking industry.
+								</p>
+								<p>I focus on writing clean, elegant and efficient code.</p>
+								<p>Experienced in building enterprise software applications.</p>
+								<p>
+									Learn the basics and the pillars of programming, and your code
+									will be robust and will adapt to changes more quickly. Also,
+									other programmers will be able to keep it without going crazy.
+								</p>
+								<p>
+									{' '}
+									I have good command in working with programming languages such
+									as Java, JavaScript, TypeScript and Python.
+								</p>
+								<p>
+									Experienced in building web applications through frameworks
+									such as React, Angular and Spring Boot.
+								</p>
 							</CustomText>
 						</Card.Text>
 						{/* <Button variant="primary">Go somewhere</Button> */}
